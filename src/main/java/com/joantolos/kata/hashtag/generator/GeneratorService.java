690e13b3e99e9c217a76e7bef97e608c863a124d
joan.tolos <joan.tolos@clarivate.com>
2020-02-20 10:28:01 +0100

    Generator logic implemented

--- src/main/java/com/joantolos/kata/hashtag/generator/GeneratorService.java
+++ src/main/java/com/joantolos/kata/hashtag/generator/GeneratorService.java
@@ -1,22 +1,43 @@
 package com.joantolos.kata.hashtag.generator;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class GeneratorService {
 
     private List<String> defaults;
     private List<String> randoms;
+    private Random random;
 
     public GeneratorService(Integer profileId) throws IOException {
         ConfigLoader configLoader = new ConfigLoader(profileId);
 
+        this.random = new Random();
         this.defaults = configLoader.getDefaults();
         this.randoms = configLoader.getRandoms();
     }
 
-    public String generate() {
-        return this.defaults.stream().map(defaultEntry -> "#" + defaultEntry).collect(Collectors.joining(",")).replace(",", " ");
+    public String generate(int randomsCount) {
+        return generateDefaults() + generateRandoms(randomsCount);
+    }
+
+    private String generateDefaults() {
+        return this.defaults.stream().map(defaultEntry -> "#" + defaultEntry).collect(Collectors.joining(",")).replace(",", " ") + " ";
+    }
+
+    private String generateRandoms(int randomsCount) {
+        List<String> tempRandoms = new ArrayList<>();
+
+        do {
+            String randomHashTag = randoms.get(random.nextInt(randoms.size()));
+            if (!tempRandoms.contains(randomHashTag)) {
+                tempRandoms.add("#" + randomHashTag);
+            }
+        } while (tempRandoms.size() != randomsCount);
+
+        return String.join(" ", tempRandoms);
     }
 }
--- src/test/java/com/joantolos/kata/hashtag/generator/GeneratorServiceTest.java
+++ src/test/java/com/joantolos/kata/hashtag/generator/GeneratorServiceTest.java
@@ -5,19 +5,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class GeneratorServiceTest {
 
-    private GeneratorService generatorService;
+    private List<String> randoms;
+    private String[] hashTags;
+    private int randomCounts;
 
     @Before
     public void setUp() throws IOException {
-        this.generatorService = new GeneratorService(1);
+        this.randomCounts = 3;
+        this.randoms = new ConfigLoader(1).getRandoms();
+        this.hashTags = new GeneratorService(1).generate(randomCounts).split(" ");
     }
 
     @Test
     public void shouldGenerateHashTagsStartingWithDefaults() {
-        Assert.assertTrue(this.generatorService.generate().startsWith("#java #programming"));
+        Assert.assertEquals("#java", hashTags[0]);
+        Assert.assertEquals("#programming", hashTags[1]);
+    }
+
+    @Test
+    public void shouldContainAtLeastOneValidRandom() {
+        Assert.assertTrue(randoms.contains(hashTags[2].replace("#", "")));
+    }
+
+    @Test
+    public void shouldContainThreeRandoms() {
+        int defaultCounts = 2;
+        Assert.assertEquals( defaultCounts + randomCounts, hashTags.length);
     }
 
 }
